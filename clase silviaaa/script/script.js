05cb5e9cc82a31ced54f5877f4a5a137243273ed
sofinista <105441648+sofinista@users.noreply.github.com>
2022-11-05 13:37:50 -0500

    Add files via upload

---
+++ clase silviaaa/script/script.js
@@ -0,0 +1,14 @@
+const panel = document.querySelectorAll('.panel');
+
+panel.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClass()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClass() {
+    panel.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
